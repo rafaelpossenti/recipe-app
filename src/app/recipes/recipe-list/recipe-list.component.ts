9e5390a762be02be7b16d465d2be6176ad849699
Rafael Possenti <rafael_possenti@hotmail.com>
2018-05-18 19:38:28 -0100

    Add shopping list detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+      new Recipe("Atum ", "Atum caramelizado com cebola",
+                 "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg"),
+      new Recipe("Frango", "Frango xadrez feito com muito carinho",
+                 "https://www.receitadevovo.com.br/gbau/sistema/receitas/img/frango-a-passarinho.jpg"),
+  ];    
+        
   constructor() { }
 
   ngOnInit() {
