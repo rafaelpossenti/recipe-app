972f8a1d70d9545c1d3c7c794e999ea452544cac
Rafael Possenti <rafael_possenti@hotmail.com>
2018-06-09 19:31:58 -0100

    add service in shopping-list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,37 @@
-import { Recipe } from './recipe.model';
+import { EventEmitter, Injectable } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+
+@Injectable()
 export class RecipeService{
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
       new Recipe("Atum ", "Atum caramelizado com cebola",
-                 "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg"),
+                 "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg",
+                 [
+                    new Ingredient('Meat',1),
+                    new Ingredient('French Fries',20)
+                 ]),
       new Recipe("Frango", "Frango xadrez feito com muito carinho",
-                 "https://www.receitadevovo.com.br/gbau/sistema/receitas/img/frango-a-passarinho.jpg")
+                 "https://www.receitadevovo.com.br/gbau/sistema/receitas/img/frango-a-passarinho.jpg",
+                 [
+                    new Ingredient('Buns',2),
+                    new Ingredient('Meat',1)
+                 ])
     ];
+
+    constructor(private slService: ShoppingListService){}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients : Ingredient[]){
+      this.slService.addIngredients(ingredients);
     }
 
 }
